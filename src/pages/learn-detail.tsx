ec7fbd26acc1f9c80bf16ee82cd15cf3581736df
abhishekya6142-web <abhishekya6142@gmail.com>
2026-05-30 14:16:03 +0530

    Update learn-detail.tsx

--- src/pages/learn-detail.tsx
+++ src/pages/learn-detail.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { ArrowLeft, Clock, ChevronRight, BookOpen } from "lucide-react";
+import { useLocation, useParams } from "wouter";
 import { Link, useLocation, useParams } from "wouter";
 
 const LEARN_TOPICS = [
